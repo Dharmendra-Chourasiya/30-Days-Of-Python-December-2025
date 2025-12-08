78b4f13a15e96a30352110efad675294717f86a1
Dharmendra Chourasiya <dharm@Dharmendras-MacBook-Air.local>
2025-12-08 14:55:09 +0530

    08-12-2025

--- 02_Day_Dec_2025/02_Day_set_methods.py
+++ 02_Day_Dec_2025/02_Day_set_methods.py
@@ -62,7 +62,7 @@
 # set.union(set2) #combines both set values & returns new
 # set.intersection(set2) #combines common values & returns new
 
-set1 = {1,2,3,4,2,3,4}
-set2 = {3,4,5,9,8,3}
-print(set1.union(set2))
-print(set1.intersection(set2))
+# set1 = {1,2,3,4,2,3,4}
+# set2 = {3,4,5,9,8,3}
+# print(set1.union(set2))
+# print(set1.intersection(set2))
---
+++ 03_Day_Dec_2025/union_intersection.py
@@ -0,0 +1,15 @@
+# set.union(set2) #combines both set values & returns new
+# set.intersection(set2) #combines common values & returns new
+
+# set1 = {1,2,3,4,2,3,4}
+# set2 = {3,4,5,9,8,3}
+# print(set1.union(set2))
+# print(set1.intersection(set2))
+
+set1 = {1,2,3}
+set2 = {2,3,4}
+print("Union of two set : ",set1.union(set2))
+print(set1)
+print(set2)
+
+print("Intersection of two set : ",set1.intersection(set2))
